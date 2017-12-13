246a43603396797477de6a71bfbb0e84bfc61a42
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-12-12 19:38:16 -0500

    changes to task

--- pages/show_task.php
+++ pages/show_task.php
@@ -1,32 +1,23 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
+<?php include 'headerPage.php' ?>
 
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
+//print_r($data);
+session_start();
 ?>
-<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
+<?php print utility\htmlTable::generateTableForOneTodo($data);
+
+?>
+<form action="index.php?page=tasks&action=edit&id=<?php echo $data->id; ?> " method="post" id="form1" style="float:left;">
+    <div class="col-lg-10"><button class="btn btn-primary text-center" type="submit" form="form1" value="edit">Edit</button></div>
 </form>
 
 
+<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2" style="float:left;">
+
+
+    <div ><button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button><a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
+</form>
 
 
 <script src="js/scripts.js"></script>
