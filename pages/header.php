a0fa65cfdc9da91cd0a8dd8b0d55727220692875
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-12-16 23:04:54 -0500

    changes to header file

---
+++ pages/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>WELCOME!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="css/main.css" rel="stylesheet">
+
+    <style>
+
+        .navbar-custom {
+        {
+            background-color:#219799;
+            color:#ffffff;
+            border-radius:0;
+        }
+
+    </style>
+
+</head>
+<body>
+
+<div class="navbar navbar-custom">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="https://web.njit.edu/~oe52/a/index.php?page=tasks&action=all">Homepage</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+
+
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?page=accounts&action=profile">Profile</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?page=tasks&action=create">Create Task</a>
+            </li>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?page=homepage&action=logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</div>
+
+<main role="main" class="container">
+
+    <div class="starter-template">
